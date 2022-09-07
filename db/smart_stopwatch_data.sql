76555b7587f94e7bf7bd1f64d026f3cbea850b95
Erlend Nukke <erlend.nukke@gmail.com>
2022-09-07 14:56:25 +0300

    database added create and data

--- db/smart_stopwatch_data.sql
+++ db/smart_stopwatch_data.sql
@@ -1,10 +1,10 @@
-INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'kadi', '123')
-INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'erlend', '123')
-INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'pavel', '123')
+INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'kadi', '123');
+INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'erlend', '123');
+INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'pavel', '123');
 
-INSERT INTO public.role (id, role) VALUES (DEFAULT, 'admin')
-INSERT INTO public.role (id, role) VALUES (DEFAULT, 'customer')
+INSERT INTO public.role (id, role) VALUES (DEFAULT, 'admin');
+INSERT INTO public.role (id, role) VALUES (DEFAULT, 'customer');
 
-INSERT INTO public.user_role (id, role_id, user_id) VALUES (DEFAULT, 1, 1)
-INSERT INTO public.user_role (id, role_id, user_id) VALUES (DEFAULT, 1, 2)
+INSERT INTO public.user_role (id, role_id, user_id) VALUES (DEFAULT, 1, 1);
+INSERT INTO public.user_role (id, role_id, user_id) VALUES (DEFAULT, 1, 2);
 INSERT INTO public.user_role (id, role_id, user_id) VALUES (DEFAULT, 2, 3)
