13674872a806def475bc407e7607758ff71beb60
Pavel Narõškin <pavel.naroskin@gmail.com>
2022-09-12 09:35:05 +0300

    Develop add Athlete WIP

--- db/smart_stopwatch_data.sql
+++ db/smart_stopwatch_data.sql
@@ -5,3 +5,9 @@ INSERT INTO public.role (id, role) VALUES (DEFAULT, 'customer');
 INSERT INTO public."user" (id, user_name, password, role_id) VALUES (DEFAULT, 'pavel', '123', 2);
 INSERT INTO public."user" (id, user_name, password, role_id) VALUES (DEFAULT, 'kadi', '123', 2);
 INSERT INTO public."user" (id, user_name, password, role_id) VALUES (DEFAULT, 'erlend', '123', 2);
+
+
+INSERT INTO public.athlete (id, user_id, name) VALUES (DEFAULT, 1, 'marleen');
+INSERT INTO public.athlete (id, user_id, name) VALUES (DEFAULT, 1, 'mia');
+INSERT INTO public.athlete (id, user_id, name) VALUES (DEFAULT, 2, 'sander');
+INSERT INTO public.athlete (id, user_id, name) VALUES (DEFAULT, 3, 'grete');
