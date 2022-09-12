faa0e3b5ff6923477ff2de3d8e29ddbedc74dc43
Pavel Narõškin <pavel.naroskin@gmail.com>
2022-09-12 14:05:36 +0300

    Develop added search athlete

--- src/main/java/com/project/smartStopWatch/app/athlete/AthleteController.java
+++ src/main/java/com/project/smartStopWatch/app/athlete/AthleteController.java
@@ -3,7 +3,6 @@ package com.project.smartStopWatch.app.athlete;
 import com.project.smartStopWatch.domain.athlete.AthleteService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
