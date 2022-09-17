232e5b2af98f24c89063dcc3aee5ae0a4d38dd82
Rain Tüür <rain.tuur@gmail.com>
2022-09-17 15:30:34 +0300

    DEVELOP added UUID to HeatRow

--- src/main/java/com/project/smartStopWatch/app/stopper/dto/dashboard/HeatRow.java
+++ src/main/java/com/project/smartStopWatch/app/stopper/dto/dashboard/HeatRow.java
@@ -3,9 +3,11 @@ package com.project.smartStopWatch.app.stopper.dto.dashboard;
 import lombok.Data;
 
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class HeatRow {
+    private UUID uuid = UUID.randomUUID();
     private Integer heatNumber;
     private Boolean hasStarted;
     private Boolean hasFinished;
